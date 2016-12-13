fe84e795324b3f3bfaba47917012e030d0f36e68
kapaseker <panoogunker@gmail.com>
2016-12-13 09:35:02 +0800

    groovy String syntax test

--- string/str.groovy
+++ string/str.groovy
@@ -3,3 +3,32 @@ int firstDot = str.indexOf('.');
 int secondDot = str.indexOf('.',firstDot+1);
 def version = str.substring(0,secondDot);
 print version;
+print '\n';
+
+def name = '''
+apple
+google
+microsoft
+huawei
+''';
+
+print name;
+
+def stripNewLine = '''\
+remove a new line by \
+adding a backslash 
+''';
+
+print stripNewLine;
+
+print 'The Euro currency symbol: \u20AC \n';
+
+def myName = "xie peng gang";
+
+print "this is you name : ${myName}.\n";
+
+def person = [name:"Qian\'s",age:23];
+
+print "$person.name has a age of $person.age \n";
+
+print "1 add 2 is ${1+2}.\n";
