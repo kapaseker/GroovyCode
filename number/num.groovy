4a570ba4af8c3191a902c52b3c82cb017ad8731b
kapaseker <panoogunker@gmail.com>
2016-12-19 21:22:43 +0800

    add number and string quote test

---
+++ number/num.groovy
@@ -0,0 +1,18 @@
+int a = 0;
+println a;
+// so strange , it doesn't auto convert to long;
+def val = Integer.MAX_VALUE + 1;
+println val;
+
+println val instanceof Integer;
+val += 1;
+println val as long;
+
+def longVal = 2147483648;
+println longVal;
+println longVal as Integer;
+println longVal instanceof Long;
+
+//Binary literal
+def binVal = 0b111;
+println binVal;
