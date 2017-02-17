04d30734524b7cf172dbe7d5391f40bf37ed40bc
kapaseker <panoogunker@gmail.com>
2017-02-17 17:15:27 +0800

    add selfType annotation sample

--- cls/trait.groovy
+++ cls/trait.groovy
@@ -1,3 +1,5 @@
+import groovy.transform.SelfType;
+import groovy.transform.CompileStatic;
 
 def printDownln(String info){
     println info+"\u2193\u2193\u2193";
@@ -88,3 +90,95 @@ trait BaseApple {
 class FuxiApple extends Fruit implements BaseApple{};
 def apple = new FuxiApple();
 apple.shrinkJuice();
+
+// mixin
+printDownln("show Mxin");
+class Acls {
+    void speak(){
+        println("We are Acls");
+    }
+} 
+
+class Bcls {
+    private String name = "Bob Cash";
+    void walk(){
+        println("I am falling down");
+    }
+}
+Acls.metaClass.mixin Bcls;
+def clA = new Acls();
+clA.speak();
+clA.walk();
+// it works
+println clA.name;
+// not allowed to access private field
+// println clA.@name;
+// it is still not the Bcls instance;
+println (clA instanceof Bcls);
+
+// fileds are not inherited;
+trait IntCouple{
+    int x = 4;
+    int y = 9;
+    int sum(){
+        return x + y;
+        // return getX()+getY();
+    }
+} 
+
+class BaseCounple implements IntCouple {
+    int x = 2;
+    int y = 3;
+    int f(){
+        return sum();
+    }
+}
+// always use IntCouple's fileds,use fuction is right
+println ((new BaseCounple()).f());
+
+printDownln("show selftype restriction");
+class CommunicationService {
+    static void sendMessage(String from, String to, String message) {       
+        println "$from sent [$message] to $to"
+    }
+}
+
+class Device { String id }                                                  
+
+@SelfType(Device)
+@CompileStatic
+trait Communicating {
+    void sendMessage(Device to, String message) {
+        CommunicationService.sendMessage(id, to.id, message)                
+    }
+}
+
+class MyDevice extends Device implements Communicating {}
+class TaDevice extends Device implements Communicating {}                   
+
+def bob = new MyDevice(id:'Bob')
+def alice = new MyDevice(id:'Alice')
+bob.sendMessage(alice,'secret')
+
+def ta = new TaDevice(id:"tacheng");
+
+ta.sendMessage(alice,'not good');
+
+// not allowed the prefix or postfix
+trait Counting {
+    int x = 0;
+    void inc() {
+        // not allowed
+        // x++
+        x+=1;                             
+    }
+    void dec() {
+        // not allowed;
+        // --x;
+        x-=1;                 
+    }
+}
+class Counter implements Counting {}
+def c = new Counter();
+c.inc();
+println c.x;
